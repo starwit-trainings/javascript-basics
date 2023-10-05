a463a8875da09c49809d158c83155459a148eb20
flonix8 <flstanek@googlemail.com>
2023-10-05 15:55:17 +0200

    Add exercise 2 challenge solution

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 "use strict";
 
+let cooldownTime = 2000;
+
 const helloWorldButton = document.querySelector("#hello-world");
 const resetButton = document.querySelector("#reset");
 
+let buttonCooldownActive = false;
+
 helloWorldButton.addEventListener("click", () => {
+    if (buttonCooldownActive) return;
+    
     helloWorldButton.disabled = true;
     helloWorldButton.textContent = "clicked";
 });
 
 resetButton.addEventListener("click", () => {
     helloWorldButton.disabled = false;
-    helloWorldButton.textContent = "Hello World";
+    
+    buttonCooldownActive = true;
+    let cooldownStartTime = Date.now();
+
+    const cooldownIntervalHandle = setInterval(() => {
+        const cooldownCountdown = cooldownTime - (Date.now() - cooldownStartTime);
+        helloWorldButton.textContent = `Wait for ${(cooldownCountdown/1000).toFixed(2)}s...`;
+    }, 50);
+    
+    setTimeout(() => {
+        buttonCooldownActive = false;
+        clearInterval(cooldownIntervalHandle);
+        helloWorldButton.textContent = "Hello World";
+    }, 2000);
+
 });
