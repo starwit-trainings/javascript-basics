2b0d420b60804ef83999616a5be8be3c23c965f2
flonix8 <flstanek@googlemail.com>
2023-10-12 17:15:31 +0200

    Extend events miniproject

--- events/script.js
+++ events/script.js
@@ -1,7 +1,19 @@
-const siteElements = document.querySelectorAll('html, body, div, button');
+const siteElements = document.querySelectorAll("html, body, div, button, a");
 
 for (const elem of siteElements) {
     elem.addEventListener("click", event => {
-        console.log(`${elem.nodeName} (id=${elem.id}) clicked. target=${event.target.id || event.target.nodeName}, currentTarget=${event.currentTarget.id || event.currentTarget.nodeName}`);
+        console.log(`Bubble: ${elem.nodeName} id=${elem.id} target=${event.target.id} currentTarget=${event.currentTarget.id}`);
+        console.log(event);
     });
 }
+
+const span = document.querySelector("#tooltip");
+
+let mouseEventCount = 0;
+
+document.querySelector("html").addEventListener("mousemove", event => {
+    // span.style.top = `${event.clientY}px`;
+    // span.style.left = `${event.clientX}px`;
+    mouseEventCount++;
+    span.textContent = `x=${event.clientX} y=${event.clientY} eventCount=${mouseEventCount}`;
+});
